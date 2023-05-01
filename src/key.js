87e3cf96596826088540cd2ecf2923f2279552a6
Serg Vergun <1197314+severgun@users.noreply.github.com>
2023-05-01 16:25:57 +0300

    refactor: move Key component creation to constructor

--- src/key.js
+++ src/key.js
@@ -57,56 +57,51 @@ export default class Key {
         this.keyText = null;
         break;
     }
-  }
 
-  createComponent() {
-    let keyComponent = null;
     if (this.code === 'placeholder') {
-      keyComponent = document.createElement('div');
-      keyComponent.id = this.id;
-      keyComponent.classList.add('keyboard__placeholder');
+      this.keyComponent = document.createElement('div');
+      this.keyComponent.id = this.id;
+      this.keyComponent.classList.add('keyboard__placeholder');
     } else {
-      keyComponent = document.createElement('button');
-      keyComponent.id = this.id;
-      keyComponent.classList.add('keyboard__key');
-      keyComponent.type = 'button';
+      this.keyComponent = document.createElement('button');
+      this.keyComponent.id = this.id;
+      this.keyComponent.classList.add('keyboard__key');
+      this.keyComponent.type = 'button';
 
       switch (this.code) {
         case 'Backspace':
-          keyComponent.classList.add('keyboard__backspace');
+          this.keyComponent.classList.add('keyboard__backspace');
           break;
         case 'Tab':
-          keyComponent.classList.add('keyboard__tab');
+          this.keyComponent.classList.add('keyboard__tab');
           break;
         case 'Backslash':
-          keyComponent.classList.add('keyboard__backslash');
+          this.keyComponent.classList.add('keyboard__backslash');
           break;
         case 'CapsLock':
-          keyComponent.classList.add('keyboard__capslock');
+          this.keyComponent.classList.add('keyboard__capslock');
           break;
         case 'Enter':
-          keyComponent.classList.add('keyboard__enter');
+          this.keyComponent.classList.add('keyboard__enter');
           break;
         case 'ShiftLeft':
-          keyComponent.classList.add('keyboard__shift-left');
+          this.keyComponent.classList.add('keyboard__shift-left');
           break;
         case 'ShiftRight':
-          keyComponent.classList.add('keyboard__shift-right');
+          this.keyComponent.classList.add('keyboard__shift-right');
           break;
         case 'Space':
-          keyComponent.classList.add('keyboard__space');
+          this.keyComponent.classList.add('keyboard__space');
           break;
         case 'ControlLeft':
         case 'OSLeft':
         case 'AltLeft':
-          keyComponent.classList.add('keyboard__ctrl-alt-shift-left');
+          this.keyComponent.classList.add('keyboard__ctrl-alt-shift-left');
           break;
         default:
           break;
       }
-      keyComponent.innerText = this.keyText;
+      this.keyComponent.innerText = this.keyText;
     }
-
-    return keyComponent;
   }
 }
